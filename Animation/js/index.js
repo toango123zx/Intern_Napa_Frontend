bbbe90208a36eeca5f08fecb276285312cae57ed
toango123zx <toango123zx@gmail.com>
2025-01-03 09:26:22 +0700

    feat: page

--- Animation/js/index.js
+++ Animation/js/index.js
@@ -8,3 +8,14 @@ const buttonSwitch = () => {
     yearCard.classList.toggle("card--hidden");
     monthCard.classList.toggle("card--hidden");
 }
+
+const question = (item) => {
+    const description = item.querySelector('.question__text--description');
+
+    // Kiểm tra và bật/tắt display
+    if (description.style.display === 'block') {
+        description.style.display = 'none';
+    } else {
+        description.style.display = 'block';
+    }
+}
